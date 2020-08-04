6b53d8612103dca90a73568bfdac6a043be76c19
meaghan jennings <meaghanljennings@gmail.com>
2020-08-04 15:36:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -122,9 +122,9 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => "C"
+          :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] << "C", "Assembly"
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
